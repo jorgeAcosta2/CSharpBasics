8a3451a97e466529e9f02dbb5cac26bc51011ca2
Xaviesta <Xaviesta@XAVIESTA-PC>
2016-08-29 19:55:19 -0400

    Starting io stream course

--- CSharpObjectsTreehouse/Game.cs
+++ CSharpObjectsTreehouse/Game.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Game
@@ -5,8 +7,16 @@ namespace TreehouseDefense
         public static void Main()
         {
             Map map = new Map(8,5);
+        
+            Point point = new Point(4, 2);
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
 
-            int area = map.Width * map.Height;
+            point = new Point(8, 5);
+            isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+
+            Console.ReadKey();
         }
     }
 }
