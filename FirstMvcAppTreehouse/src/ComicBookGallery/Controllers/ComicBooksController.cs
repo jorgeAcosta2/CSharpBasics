af4511a90a75523b0616554d73e8abb7471d444d
Xaviesta <Xaviesta@XAVIESTA-PC>
2016-09-28 03:03:43 -0400

    ActionResult lesson

---
+++ FirstMvcAppTreehouse/src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+       public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello From the comic books controller");
+           
+        }
+    }
+}
